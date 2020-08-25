3b3198ee73b7fc3b1d22d8cb4e98a5e0e904d4b7
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-08-25 15:51:32 -0400

    move gd inside the app

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const GoogleDrive = require('./googleDrive');
 const express = require('express');
 var app = express();
-const gd = new GoogleDrive({
-	client_id: '202264815644.apps.googleusercontent.com',
-	client_secret: 'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
-	refresh_token: process.env.token
-})
 app.use('/', (req, res) => {
+	const gd = new GoogleDrive({
+		client_id: '202264815644.apps.googleusercontent.com',
+		client_secret: 'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
+		refresh_token: process.env.token
+	})
 	var url = req.originalUrl.replace('/', '');
 	const result = gd.getMeta(url);
 	const newurl = result.webViewLink;
