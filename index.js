c05aca03715f51f2cb7ff012e2ed7cc01f8a9a2c
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-08-25 18:26:15 -0400

    refactor (#7)
    * debug
    
    * cancel await
    
    * Revert "cancel await"
    
    This reverts commit 421fd670565e4d00be9a8a273a87e928b12590c8.
    
    * add await
    
    * remove unused function
    
    * async
    
    * use require instead of import
    
    * typo
    
    * typo
    
    * fix
    
    * fix
    
    * remove await and async
    
    * move gd inside the app
    
    * refactor
    
    * fix error
    
    * fix
    
    * await
    
    * bugfix

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-import GoogleDrive from './googleDrive'
-var express = require('express');
+const {google} = require('googleapis');
+const express = require('express');
 var app = express();
-const gd = new GoogleDrive({
-	client_id: '202264815644.apps.googleusercontent.com',
-	client_secret: 'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
-	refresh_token: process.env.token
-})
-app.use('/', function (req, res) {
+
+app.use('/', async (req, res) => {
+	const oauth2Client = new google.auth.OAuth2(
+		'202264815644.apps.googleusercontent.com',
+		'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
+		''
+	);
+	oauth2Client.setCredentials({
+		  refresh_token: process.env.token
+	});
+	const drive = google.drive({
+		version: 'v3',
+		auth: oauth2Client,
+	});
 	var url = req.originalUrl.replace('/', '');
-	const result = gd.getMeta(url);
-	const newurl = result.webViewLink;
+	if (!url){
+		res.redirect(302, "https://njzjz.win/");
+		return;
+	}
+	const result = await drive.files.get({
+		fileId: url,
+		fields: 'webContentLink',
+		//alt: 'media',
+	});
+	const newurl = result.data.webContentLink;
 	res.redirect(302, newurl);
 })
 module.exports = app;
