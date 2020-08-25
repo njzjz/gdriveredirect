b4123a8d9de9bd70081cfe6ebdf4ffa97d51c4c7
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-08-25 16:10:00 -0400

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const GoogleDrive = require('./googleDrive');
+const {google} = require('googleapis');
 const express = require('express');
 var app = express();
+
 app.use('/', (req, res) => {
-	const gd = new GoogleDrive({
-		client_id: '202264815644.apps.googleusercontent.com',
-		client_secret: 'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
-		refresh_token: process.env.token
+	const oauth2Client = new google.auth.OAuth2(
+		'202264815644.apps.googleusercontent.com',
+		'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
+		''
+	);
+	oauth2Client.setCredentials({
+		  refresh_token: process.env.token
+	});
+	const drive = google.drive({
+		version: 'v3',
+		auth: oauth2Client,
 	})
 	var url = req.originalUrl.replace('/', '');
-	const result = gd.getMeta(url);
-	const newurl = result.webViewLink;
+	const result = drive.get({
+		fileId: url
+	});
+	//const newurl = result.webViewLink;
 	//res.redirect(302, newurl);
 	res.send(result);
 })
