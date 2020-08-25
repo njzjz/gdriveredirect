717570257d401ff26fae3b2cb416680ce727c66d
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-08-25 02:40:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import mime from 'mime'
+import GoogleDrive from './googleDrive'
+var express = require('express');
+var app = express();
+const gd = new GoogleDrive({
+	client_id: '202264815644.apps.googleusercontent.com',
+	client_secret: 'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
+	refresh_token: process.env.token
+})
+app.use('/', function (req, res) {
+	var url = req.originalUrl.replace('/', '');
+	const result = gd.getMeta(url);
+	const newurl = result.webViewLink;
+	res.redirect(302, newurl);
+})
+module.exports = app;
