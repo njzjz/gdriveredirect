8d7492d76ca4a0a11f8510601c9ea1efbc893f0d
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-08-25 17:21:48 -0400

    await

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {google} = require('googleapis');
 const express = require('express');
 var app = express();
 
-app.use('/', (req, res) => {
+app.use('/', async (req, res) => {
 	const oauth2Client = new google.auth.OAuth2(
 		'202264815644.apps.googleusercontent.com',
 		'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
@@ -20,14 +20,12 @@ app.use('/', (req, res) => {
 		res.redirect(302, "https://njzjz.win/");
 		return;
 	}
-	const result = drive.files.get({
+	const result = await drive.files.get({
 		fileId: url,
 		fields: 'webContentLink',
 		//alt: 'media',
 	});
-	result.then(function(res){
-		const newurl = res.data.webContentLink;
-		res.redirect(302, newurl);
-	})
+	const newurl = res.data.webContentLink;
+	res.redirect(302, newurl);
 })
 module.exports = app;
