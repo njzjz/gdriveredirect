4cfe55d314cbd8fe601db0f48b15bfa388df1089
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-08-27 21:59:14 -0400

    handle errors (#10)
    * add 404 page
    
    * catch error
    
    * add error
    
    * Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,35 @@ const express = require('express');
 var app = express();
 
 app.use('/', async (req, res) => {
-	const oauth2Client = new google.auth.OAuth2(
-		'202264815644.apps.googleusercontent.com',
-		'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
-		''
-	);
-	oauth2Client.setCredentials({
-		  refresh_token: process.env.token
-	});
-	const drive = google.drive({
-		version: 'v3',
-		auth: oauth2Client,
-	});
 	var url = req.originalUrl.replace('/', '');
 	if (!url){
 		res.redirect(302, "https://njzjz.win/");
 		return;
 	}
-	const result = await drive.files.get({
-		fileId: url,
-		fields: 'webContentLink',
-		//alt: 'media',
-	});
-	const newurl = result.data.webContentLink;
-	res.redirect(302, newurl);
+	try {
+		const oauth2Client = new google.auth.OAuth2(
+			'202264815644.apps.googleusercontent.com',
+			'X4Z3ca8xfWDb1Voo-F9a7ZxJ',
+			''
+		);
+		oauth2Client.setCredentials({
+			  refresh_token: process.env.token
+		});
+		const drive = google.drive({
+			version: 'v3',
+			auth: oauth2Client,
+		});
+		
+		const result = await drive.files.get({
+			fileId: url,
+			fields: 'webContentLink',
+			//alt: 'media',
+		})
+		const newurl = result.data.webContentLink;
+		res.redirect(302, newurl);
+	} catch (err) {
+		console.error(err);
+		res.redirect(302, `https://drive.google.com/uc?id=${url}`);
+	}
 })
 module.exports = app;
